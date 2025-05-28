4c9f24bae155f4a3f97d8e31ecd2743e5c42a47e
Max Hilbrunner <m.hilbrunner@gmail.com>
2025-05-28 18:42:29 +0200

    Update to Foundry v13

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ Hooks.on('init', () => {
         type: Boolean,
         default: true
     });
-    game.settings.register("token-hud-wildcard", "imageOpacity", {
+    game.settings.register('token-hud-wildcard', 'imageOpacity', {
         name: game.i18n.format('THWildcard.OpacitySettingName'),
         hint: game.i18n.format('THWildcard.OpacitySettingHint'),
-        scope: "client",
+        scope: 'client',
         config: true,
         range: { min: 0, max: 100, step: 1 },
         type: Number,
@@ -51,7 +51,7 @@ Hooks.on('init', () => {
 });
 
 Hooks.on('ready', () => {
-    Hooks.on('renderTokenConfig', renderTokenConfig);
+    Hooks.on('renderPrototypeTokenConfig', renderTokenConfig);
     Hooks.on('preCreateToken', (parent, data, options, userId) => {
         const defaultValue = parent?.actor?.prototypeToken?.getFlag('token-hud-wildcard', 'default');
         if (defaultValue && parent?.actor?.prototypeToken?.randomImg) {
@@ -65,28 +65,28 @@ async function renderTokenConfig(config, html) {
     if (config.token._id) {
         return;
     }
-    const imageDataTab = html.find('.tab[data-tab="appearance"],.tab[data-tab="image"]');
-    const checkBoxWildcard = imageDataTab.find('input[name="randomImg"]');
+    const imageDataTab = html.querySelector('.tab[data-tab="appearance"],.tab[data-tab="image"]');
+    const checkBoxWildcard = imageDataTab?.querySelector('input[name="randomImg"]');
     const configField = await renderTemplate(
         '/modules/token-hud-wildcard/templates/configField.html',
-        { defaultImg, available: checkBoxWildcard[0].checked });
+        { defaultImg, available: checkBoxWildcard?.checked });
 
-    imageDataTab.append(configField);
-    const defaultConfig = imageDataTab.find('.thwildcard-default');
-
-    defaultConfig.find('button').click(event => {
+    imageDataTab?.insertAdjacentHTML('beforeend', configField);
+    const defaultConfig = imageDataTab.querySelector('.thwildcard-default');
+    const defaultImgButton = defaultConfig?.querySelector('button');
+    defaultImgButton?.addEventListener('click', (event) => {
         event.preventDefault();
-        const input = defaultConfig.find('input')[0];
-
+        const input = defaultConfig.querySelector('input');
+        if (!input) return;
         const fp = new FilePicker({ current: input.value, field: input });
         fp.browse(defaultImg);
     });
 
-    checkBoxWildcard.click(event => {
+    checkBoxWildcard?.addEventListener('click', event => {
         if (event.target.checked) {
-            defaultConfig[0].classList.add('active');
+            defaultConfig.classList.add('active');
         } else {
-            defaultConfig[0].classList.remove('active');
+            defaultConfig.classList.remove('active');
         }
     });
 }
@@ -110,68 +110,72 @@ Hooks.on('renderTokenHUD', async (app, html, context) => {
     const imageOpacity = game.settings.get('token-hud-wildcard', 'imageOpacity') / 100;
     const wildcardDisplay = await renderTemplate('/modules/token-hud-wildcard/templates/hud.html', { images, imageDisplay, imageOpacity });
 
-    html.find('div.right')
-        .append(wildcardDisplay)
-        .click((event) => {
-            let activeButton, clickedButton, tokenButton;
-            for (const button of html.find('div.control-icon')) {
-                if (button.classList.contains('active')) activeButton = button;
-                if (button === event.target.parentElement) clickedButton = button;
-                if (button.dataset.action === 'thwildcard-selector') tokenButton = button;
-            }
+    let right = html.querySelector('div.right');
+    right?.insertAdjacentHTML('beforeend', wildcardDisplay);
+    right?.addEventListener('click', async (event) => {
+        let activeButton, clickedButton, tokenButton;
+        for (const button of html.querySelectorAll('div.control-icon')?.values()) {
+            if (button.classList.contains('active')) activeButton = button;
+            if (button === event.target.parentElement) clickedButton = button;
+            if (button.dataset.action === 'thwildcard-selector') tokenButton = button;
+        }
 
-            if (clickedButton === tokenButton && activeButton !== tokenButton) {
-                tokenButton.classList.add('active');
-                html.find('.thwildcard-selector-wrap')[0].classList.add('active');
-                const effectSelector = '[data-action="effects"]';
-                html.find(`.control-icon${effectSelector}`)[0].classList.remove('active');
-                html.find('.status-effects')[0].classList.remove('active');
-            } else {
-                tokenButton.classList.remove('active');
-                html.find('.thwildcard-selector-wrap')[0].classList.remove('active');
-            }
-        })
-        .contextmenu(async (event) => {
-            if (event.target?.parentElement?.dataset?.action !== 'thwildcard-selector') {
-                return;
-            }
-            if (!game.settings.get('token-hud-wildcard', 'rightClickRandomize')) {
-                return;
-            }
+        if (clickedButton === tokenButton && activeButton !== tokenButton) {
+            tokenButton.classList.add('active');
+            html.querySelector('.thwildcard-selector-wrap')?.classList.add('active');
+            const effectSelector = '[data-action="effects"]';
+            html.querySelector(`.control-icon${effectSelector}`)?.classList.remove('active');
+            html.querySelector('.status-effects')?.classList.remove('active');
+        } else {
+            tokenButton.classList.remove('active');
+            html.querySelector('.thwildcard-selector-wrap')?.classList.remove('active');
+        }
+    });
+    right.addEventListener('contextmenu', async (event) => {
+        if (event.target?.parentElement?.dataset?.action !== 'thwildcard-selector') {
+            return;
+        }
+        if (!game.settings.get('token-hud-wildcard', 'rightClickRandomize')) {
+            return;
+        }
+        event.preventDefault();
+        event.stopPropagation();
+        const randomImg = await getRandomTokenImage(token);
+        if (randomImg?.route) {
+            updateTokenImage(token, randomImg.route);
+        }
+    });
+
+    const buttons = html.querySelectorAll('.thwildcard-button-select');
+
+    for (const button of buttons?.values()) {
+        const imgName = button.querySelector('span, .thwildcard-button-image')?.dataset?.name;
+        if (!imgName) continue;
+
+        button.addEventListener('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
-            const randomImg = await getRandomTokenImage(token);
-            if (randomImg?.route) {
-                updateTokenImage(token, randomImg.route);
-            }
+            updateTokenImage(token, imgName);
         });
 
-    const buttons = html.find('.thwildcard-button-select');
-
-    buttons.map((button) => {
-        buttons[button].addEventListener('click', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            updateTokenImage(token, event.target.dataset.name)
-        });
-        buttons[button].addEventListener('contextmenu', function (event) {
+        button.addEventListener('contextmenu', function (event) {
             if (!game.settings.get('token-hud-wildcard', 'rightClickShare')) {
                 return;
             }
             event.preventDefault();
             event.stopPropagation();
-            new ImagePopout(event.target.dataset.name, {
+            new ImagePopout(imgName, {
                 title: token.name,
                 shareable: token.actor?.isOwner ?? game.user?.isGM,
                 uuid: token.actor?.uuid
             }).render(true);
         });
-    });
+    }
 });
 
 function updateTokenImage(token, img, opts = { updateSource: false, updateDocument: true }) {
     let updateInfo = getTokenDimensions(token, img);
-    updateInfo["texture.src"] = img;
+    updateInfo['texture.src'] = img;
     const shouldAnimate = game.settings.get('token-hud-wildcard', 'animate');
 
     if (opts.updateSource) {
@@ -257,8 +261,8 @@ function getTokenDimensions(token, imgName) {
     var dimensionData = {
         height: extractNumVar(imgName, 'height', prototypeData?.height),
         width: extractNumVar(imgName, 'width', prototypeData?.width),
-        "texture.scaleX": extractNumVar(imgName, 'scale', prototypeData?.texture.scaleX),
-        "texture.scaleY": extractNumVar(imgName, 'scale', prototypeData?.texture.scaleY),
+        'texture.scaleX': extractNumVar(imgName, 'scale', prototypeData?.texture.scaleX),
+        'texture.scaleY': extractNumVar(imgName, 'scale', prototypeData?.texture.scaleY),
     }
     return dimensionData;
 }
